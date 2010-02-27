f8a313eef8372caf6e550350283cd770460e59ce
Benjamin Lamb <bennyfactor@iaatb.net>
2010-02-27 10:57:43 -0500

    fixed additions to database.sql based on dump of production DB

--- database.sql
+++ database.sql
@@ -1,9 +1,9 @@
 
 
-CREATE TABLE lil_urls (
-  id varchar(255) NOT NULL default '',
-  url text,
-  date timestamp(14) NOT NULL,
-  'user' text,
-  PRIMARY KEY  (id)
-) TYPE=MyISAM;
+CREATE TABLE  `lil_urls` (
+  `id` varchar(255) NOT NULL default '',
+  `url` text,
+  `date` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `user` text NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
