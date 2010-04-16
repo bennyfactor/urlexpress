db33d8061bcac38646834406dda34ee15c546330
Benjamin Lamb <bennyfactor@iaatb.net>
2010-04-16 15:55:27 -0400

    set `id` to collate utf8_bin for case-sensitivity

--- database.sql
+++ database.sql
@@ -1,7 +1,7 @@
 
 
 CREATE TABLE  `lil_urls` (
-  `id` varchar(255) NOT NULL default '',
+  `id` varchar(255) character set utf8 collate utf8_bin NOT NULL default '',
   `url` text,
   `date` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   `user` text NOT NULL,
